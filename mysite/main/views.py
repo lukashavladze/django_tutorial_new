2018ea6eddb5966741cb694f16086b769285305a
LSHAVLADZE <LSHAVLADZE@WS002HP290G9>
2024-07-22 15:24:50 +0400

    added templates

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,5 +5,7 @@ from . models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
